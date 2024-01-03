30637c69feade78bf7b42e926e17e74783465dcb
Margarida Ascensão Lourenço <margarida.a.lourenco@tecnico.ulisboa.pt>
2024-01-03 15:17:10 +0000

    logica do while no server (está mal, mas penso que seja algo do género)

--- proj_23-24-p2_base/server/main.c
+++ proj_23-24-p2_base/server/main.c
@@ -1,10 +1,25 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "common/constants.h"
 #include "common/io.h"
 #include "operations.h"
+
+typedef struct{
+  char req_path[40];  
+  char resp_path[40]; 
+  int session_id;
+} Client;
+
+
+int req_pipe, resp_pipe, server_pipe;
 
 int main(int argc, char* argv[]) {
   if (argc < 2 || argc > 3) {
@@ -30,15 +45,67 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
+  char* server_pipename = argv[1];
   //TODO: Intialize server, create worker threads
-  
+  if (unlink(server_pipename) != 0 /*&& errno != ENOENT*/) {
+      printf("Unlinking pipe \n");
+      return -1;
+    }
 
+  if (mkfifo(server_pipename, 0777) != 0) {
+    printf("Error creating pipe \n");
+    return -1;
+  }
+
+  server_pipe = open(server_pipename, O_RDONLY);
+  if (server_pipe == -1) {
+    printf("Error opening pipe\n");
+    return -1;
+  }
+
+  Client previous_client;
   while (1) {
     //TODO: Read from pipe
     //TODO: Write new client to the producer-consumer buffer
+    //Client newClient;
+    char buffer[512];
+    if (read(server_pipe, buffer, 512) == -1) {
+      printf("Error reading from pipe\n");
+      return -1;
+    }
+    ssize_t op_code = buffer[0];
+    switch (op_code){
+      case '1':
+        char req_pipename[41], resp_pipename[41];
+        strncpy(req_pipename, buffer + 1, 40);
+        strncpy(resp_pipename, buffer + 41, 40);
+        ems_setup(req_pipename, resp_pipename, server_pipename);
+        break;
+
+      case '2':
+        ems_quit();
+        break;
+
+      case '3':
+        char event_id = buffer[1], num_rows = buffer[2], num_cols = buffer[3];
+        ems_create(event_id, num_rows, num_cols);
+        break;
+
+      case '4':
+        char event_id = buffer[1], num_seats = buffer[2], xs = buffer[3], ys = buffer[4];
+        ems_reserve(event_id, num_seats, xs, ys);
+        break;
+
+      case '5':
+        char event_id = buffer[1];
+        ems_show(out_fd, event_id);
+        break;
+        
+      case '6':
+        ems_list_events(out_fd);
+        break;
+      }
   }
-
   //TODO: Close Server
-
   ems_terminate();
 }
